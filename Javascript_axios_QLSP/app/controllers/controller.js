4a71ccc968654c5430fca9dafb43302969ef9bb8
ThyNguyen <thynguyen.tn293@gmail.com>
2024-11-02 14:03:23 +0700

    update

--- Javascript_axios_QLSP/app/controllers/controller.js
+++ Javascript_axios_QLSP/app/controllers/controller.js
@@ -1,9 +1,9 @@
 function renderProductList(productsList) {
-  var content = "";
-  for (var i = 0; i < productsList.length; i++) {
-    var product = productsList[i];
+    var content = "";
+    for (var i = 0; i < productsList.length; i++) {
+        var product = productsList[i];
 
-    var contentTr = `
+        var contentTr = `
         <tr>
             <td>${product.id}</td>
             <td>${product.name}</td>
@@ -17,36 +17,43 @@ function renderProductList(productsList) {
         </tr>
     `;
 
-    content += contentTr;
-  }
+        content += contentTr;
+    }
 
-  //   in danh sách ra giao diện
-  document.querySelector("#tblDanhSachSP").innerHTML = content;
+    //   in danh sách ra giao diện
+    document.querySelector("#tblDanhSachSP").innerHTML = content;
 }
 
 function getInfo() {
-  var id = document.getElementById("MaSP").value;
-  var name = document.getElementById("TenSP").value;
-  var price = document.getElementById("GiaSP").value;
-  var image = document.getElementById("HinhSP").value;
-  var description = document.getElementById("MoTaSP").value;
+    var id = document.getElementById("MaSP").value;
+    var name = document.getElementById("TenSP").value;
+    var price = document.getElementById("GiaSP").value;
+    var image = document.getElementById("HinhSP").value;
+    var description = document.getElementById("MoTaSP").value;
 
-  return new Product(id, name, price, image, description);
+    return new Product(id, name, price, image, description);
+
+    // let product = new Product();
+    // let arrInput = document.querySelectorAll("#foodForm input");
+    // for (let input of arrInput) {
+    //     console.log("input: ", input.id, input.value);
+    //     product[input.id] = input.value;
+    // }
 }
 
 // reset form
 function resetForm() {
-  document.querySelector("#MaSP").value = "";
-  document.querySelector("#TenSP").value = "";
-  document.querySelector("#GiaSP").value = "";
-  document.querySelector("#HinhSP").value = "";
-  document.querySelector("#MoTaSP").value = "";
+    document.querySelector("#MaSP").value = "";
+    document.querySelector("#TenSP").value = "";
+    document.querySelector("#GiaSP").value = "";
+    document.querySelector("#HinhSP").value = "";
+    document.querySelector("#MoTaSP").value = "";
 }
 
 function onLoading() {
-  document.querySelector("#spinner").style.display = "flex";
+    document.querySelector("#spinner").style.display = "flex";
 }
 
 function offLoading() {
-  document.querySelector("#spinner").style.display = "none";
+    document.querySelector("#spinner").style.display = "none";
 }
